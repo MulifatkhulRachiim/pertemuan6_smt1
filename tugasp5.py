1bada9499192c0e3e7bbe90a08317631c2f9c0a6
mulifarach <mulifatkhul.rach@gmail.com>
2020-11-06 22:40:00 +0700

    update folder

---
+++ tugasp5.py
@@ -0,0 +1,25 @@
+print("please enter full name : ")
+fullname=input()
+
+print("please insert your nickname : ")
+nickname=input()
+
+print("please enter your NPM : ")
+NPM=input()
+
+print("please enter Born place : ")
+bornplace=input()
+
+print("please insert your age : ")
+age=input()
+
+print("please enter your home addres : ")
+address=inpu()
+
+print("please enter ypur phone number : ")
+phonenumber=input()
+
+print("\nAssalamualaikum Wr, Wb.")
+print(f"\nlet me introduce my self. My name is {fullname}, but you call me {nickname}. My NPM is {NPM}, I was Born in {bornplace} and i am {age} years old. I am very glad if you want to invite my house in {addres}. so, don't forget to call me before with the number {phonenumber}.")
+print("\ nThank you,")
+print("\n")
